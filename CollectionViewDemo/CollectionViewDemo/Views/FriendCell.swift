c8661d88c454700ee05f799f25111a9b6279f685
Brett Markowitz <batting1000@comcast.net>
2020-01-08 13:09:19 -0500

    Added FriendCell subview constraints

--- CollectionViewDemo/CollectionViewDemo/Views/FriendCell.swift
+++ CollectionViewDemo/CollectionViewDemo/Views/FriendCell.swift
@@ -24,16 +24,38 @@ class FriendCell: UICollectionViewCell {
     
     func configureImageView(with imageName: String) {
         avatarImageView = UIImageView(image: UIImage(named: imageName))
+        avatarImageView.layer.cornerRadius = 5
         addSubview(avatarImageView)
+        
+        NSLayoutConstraint.activate([
+            avatarImageView.topAnchor.constraint(equalTo: topAnchor, constant: 5),
+            avatarImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 5),
+            avatarImageView.heightAnchor.constraint(equalToConstant: 20),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 20)
+        ])
     }
     
     func configureTitleLabel(with text: String) {
         titleLabel = FriendLabel(text: text, with: .preferredFont(forTextStyle: .headline))
         addSubview(titleLabel)
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 5),
+            titleLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 10),
+            titleLabel.heightAnchor.constraint(equalToConstant: 20),
+            titleLabel.widthAnchor.constraint(equalToConstant: 100)
+        ])
     }
     
     func configureSubtitleLabel(with text: String) {
         subtitleLabel = FriendLabel(text: text, with: .preferredFont(forTextStyle: .subheadline))
         addSubview(subtitleLabel)
+        
+        NSLayoutConstraint.activate([
+            subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 5),
+            subtitleLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 10),
+            subtitleLabel.heightAnchor.constraint(equalToConstant: 20),
+            subtitleLabel.widthAnchor.constraint(equalToConstant: 100)
+        ])
     }
 }
